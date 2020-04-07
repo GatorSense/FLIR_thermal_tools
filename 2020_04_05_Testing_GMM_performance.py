87cecb44f16d80f67bf56cc2c205277ed9d80655
Sofia Vega <svega@stargate.datasci.danforthcenter.org>
2020-04-06 22:50:51 -0500

    Adding Testing of Classification methods and updating utilities with GMM

---
+++ 2020_04_05_Testing_GMM_performance.py
@@ -0,0 +1,139 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr  5 11:43:09 2020
+
+@author: sofiavega
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.use('TKAgg') # Needed to have figures display properly in spyder. 
+import flirimageextractor
+import FLIR_thermal_tools.utilities as u
+import cv2
+from scipy import ndimage
+from sklearn.mixture import GaussianMixture
+from mpl_toolkits.mplot3d import Axes3D # <--- This is important for 3d plotting 
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import cm
+from matplotlib import colors
+from sklearn.cluster import spectral_clustering
+
+##Testing the 2019-05-05 data set
+## Load Image using flirimageextractor
+# Note: I had to change the path of my exiftool which you may need to also change.
+filename = 'C:\\Users\\sofiavega\\FLIR_thermal_tools\\FLIR5424.jpg'
+flir = flirimageextractor.FlirImageExtractor(exiftool_path="C:\\Users\\sofiavega\\AppData\\Local\\Temp\\Temp1_exiftool-11.91.zip\\exiftool(-k)")
+flir.process_image(filename, RGB=True)
+
+## Examine thermal and full resolution RGB images
+# Most FLIR cameras take a thermal image and a corresponding RGB image. 
+# The RGB camera is higher resolution and has a larger field of view. 
+therm = flir.get_thermal_np()
+rgb_fullres = flir.get_rgb_np()
+plt.figure(figsize=(10,5))
+plt.subplot(1,2,1)
+plt.imshow(therm)
+plt.title('Thermal Image')
+plt.subplot(1,2,2)
+plt.imshow(rgb_fullres)
+plt.title('RGB Full Resolution Image')
+plt.show(block='TRUE') # I needed to have block=TRUE for image to remain displayed
+
+## Check how well thermal and rgb registration is without manually correction
+# You can see that the images do not line up and there is an offset even after 
+# correcting for offset provided in file header
+#The rgb cropped seems pretty close
+rgb_lowres, rgb_crop = u.extract_rescale_image(flir)
+
+GMM_rgb(image=rgb_crop,num_class=3,hsv = 0, plot=1)
+
+###############################################################################
+##Testing the 2019-02-04 Data Set
+## Load Image using flirimageextractor
+# Note: I had to change the path of my exiftool which you may need to also change.
+filename = 'C:\\Users\\sofiavega\\FLIR_thermal_tools\\FLIR0346.jpg'
+flir = flirimageextractor.FlirImageExtractor(exiftool_path="C:\\Users\\sofiavega\\AppData\\Local\\Temp\\Temp1_exiftool-11.91.zip\\exiftool(-k)")
+flir.process_image(filename, RGB=True)
+
+## Examine thermal and full resolution RGB images
+# Most FLIR cameras take a thermal image and a corresponding RGB image. 
+# The RGB camera is higher resolution and has a larger field of view. 
+therm = flir.get_thermal_np()
+rgb_fullres = flir.get_rgb_np()
+plt.figure(figsize=(10,5))
+plt.subplot(1,2,1)
+plt.imshow(therm)
+plt.title('Thermal Image')
+plt.subplot(1,2,2)
+plt.imshow(rgb_fullres)
+plt.title('RGB Full Resolution Image')
+plt.show(block='TRUE') # I needed to have block=TRUE for image to remain displayed
+
+## Check how well thermal and rgb registration is without manually correction
+# You can see that the images do not line up and there is an offset even after 
+# correcting for offset provided in file header
+#The rgb cropped seems pretty close
+rgb_lowres, rgb_crop = u.extract_rescale_image(flir)
+
+GMM_rgb(image=rgb_crop,num_class=3,hsv = 0, plot=1)
+
+
+###############################################################################
+##Testing the 2019-02-04 Data Set
+## Load Image using flirimageextractor
+# Note: I had to change the path of my exiftool which you may need to also change.
+filename = 'C:\\Users\\sofiavega\\FLIR_thermal_tools\\FLIR0138.jpg'
+flir = flirimageextractor.FlirImageExtractor(exiftool_path="C:\\Users\\sofiavega\\AppData\\Local\\Temp\\Temp1_exiftool-11.91.zip\\exiftool(-k)")
+flir.process_image(filename, RGB=True)
+
+## Examine thermal and full resolution RGB images
+# Most FLIR cameras take a thermal image and a corresponding RGB image. 
+# The RGB camera is higher resolution and has a larger field of view. 
+therm = flir.get_thermal_np()
+rgb_fullres = flir.get_rgb_np()
+plt.figure(figsize=(10,5))
+plt.subplot(1,2,1)
+plt.imshow(therm)
+plt.title('Thermal Image')
+plt.subplot(1,2,2)
+plt.imshow(rgb_fullres)
+plt.title('RGB Full Resolution Image')
+plt.show(block='TRUE') # I needed to have block=TRUE for image to remain displayed
+
+## Check how well thermal and rgb registration is without manually correction
+# You can see that the images do not line up and there is an offset even after 
+# correcting for offset provided in file header
+#The rgb cropped seems pretty close
+rgb_lowres, rgb_crop = u.extract_rescale_image(flir)
+
+GMM_rgb(image=rgb_crop,num_class=5,hsv = 0, plot=1)
+
+###############################################################################
+##Testing the 06-21-2019 Data Set
+filename = 'C:\\Users\\sofiavega\\FLIR_thermal_tools\\FLIR7780.jpg'
+flir = flirimageextractor.FlirImageExtractor(exiftool_path="C:\\Users\\sofiavega\\AppData\\Local\\Temp\\Temp1_exiftool-11.91.zip\\exiftool(-k)")
+flir.process_image(filename, RGB=True)
+
+## Examine thermal and full resolution RGB images
+# Most FLIR cameras take a thermal image and a corresponding RGB image. 
+# The RGB camera is higher resolution and has a larger field of view. 
+therm = flir.get_thermal_np()
+rgb_fullres = flir.get_rgb_np()
+plt.figure(figsize=(10,5))
+plt.subplot(1,2,1)
+plt.imshow(therm)
+plt.title('Thermal Image')
+plt.subplot(1,2,2)
+plt.imshow(rgb_fullres)
+plt.title('RGB Full Resolution Image')
+plt.show(block='TRUE') # I needed to have block=TRUE for image to remain displayed
+
+## Check how well thermal and rgb registration is without manually correction
+# You can see that the images do not line up and there is an offset even after 
+# correcting for offset provided in file header
+#The rgb cropped seems pretty close
+rgb_lowres, rgb_crop = u.extract_rescale_image(flir)
+
+GMM_rgb(image=rgb_crop,num_class=7,hsv = 1, plot=1)
